cd93bc8b4b3a0d7bdbc35fcc6a2ed9f2e40f2103
Jackson Miller <jackson@resaleai.com>
2019-09-08 11:49:03 -0500

    Create thinkpad-popos.sh

---
+++ thinkpad-popos.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Docker
+echo "DOCKER INSTALL"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ${USER}
+echo "DOCKER INSTALL COMPLETE"
+
+# node version manager (nvm)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
